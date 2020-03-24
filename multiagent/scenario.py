983092e54154a0fc1dcbacb854a39a35c9ad8876
Oleg Grigoryev <baradist@ya.ru>
2020-03-24 17:16:00 +0300

    Make scenario return env

--- multiagent/scenario.py
+++ multiagent/scenario.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 # defines scenario upon which the world is built
+from multiagent.environment import MultiAgentEnv
+
+
 class BaseScenario(object):
     # create elements of the world
     def make_world(self):
@@ -8,3 +11,9 @@ class BaseScenario(object):
     # create initial conditions of the world
     def reset_world(self, world):
         raise NotImplementedError()
+    def get_env(self, world, reset_callback=None, reward_callback=None,
+                 observation_callback=None, info_callback=None,
+                 done_callback=None, shared_viewer=True):
+        return MultiAgentEnv(world, reset_callback, reward_callback,
+                 observation_callback, info_callback,
+                 done_callback, shared_viewer)
